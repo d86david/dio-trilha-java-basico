4acc7ff2e3b4542fa858ee1595cb91d67d12d492
d86david <david86_3@hotmail.com>
2024-05-06 11:43:05 -0300

    Inserção dos comentarios TODO:

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,18 +1,21 @@
+//TODO:"importe da classe Scanner"
 import java.util.Scanner;
 
 public class ContaTerminal {
 	//Projeto Conta Banco desenvolvido pata trilha java básico
 	
 	public static void main(String[] args ) {
-		//TODO
+		//TODO:"declaração das variaveis "
 		int numero ;
 		String agencia;
 		String nome;
 		String sobrenome;
 		double saldo;
 		
+		//TODO:"declaração e instanciação da classe Scanner"
 		Scanner sc = new Scanner(System.in);
 		
+		//TODO:"Mensagens ao usuario e obtenção dos valores digitados"
 		System.out.println("Digite o número da Agencia: ");
 		agencia = sc.next();
 		
@@ -28,6 +31,7 @@ public class ContaTerminal {
 		System.out.println("Digite o saldo da conta: ");
 		saldo = sc.nextDouble();
 		
+		//TODO:"Mensagem da conta Criada"
 		System.out.println("Olá " + nome + " " + sobrenome + ", " + "obrigado por criar uma conta em nosso banco, "
 				+ "sua agência é: " + agencia + ", "+ "conta: " + numero + 
 				" e seu saldo " + saldo + " ja está disponível para saque!");
