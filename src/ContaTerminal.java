1a61616edf11b5353c04e7d0efcced7ad94b727d
d86david <david86_3@hotmail.com>
2024-05-06 11:36:12 -0300

    Criação da classe Conta Terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
 	//Projeto Conta Banco desenvolvido pata trilha java básico
 	
 	public static void main(String[] args ) {
 		//TODO
+		int numero ;
+		String agencia;
+		String nome;
+		String sobrenome;
+		double saldo;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o número da Agencia: ");
+		agencia = sc.next();
+		
+		System.out.println("Digite o numero da conta: ");
+		numero = sc.nextInt();
+		
+		System.out.println("Digite o nome: ");
+		nome = sc.next();
+		
+		System.out.println("Digite o sobrenome: ");
+		sobrenome = sc.next();
+		
+		System.out.println("Digite o saldo da conta: ");
+		saldo = sc.nextDouble();
+		
+		System.out.println("Olá " + nome + " " + sobrenome + ", " + "obrigado por criar uma conta em nosso banco, "
+				+ "sua agência é: " + agencia + ", "+ "conta: " + numero + 
+				" e seu saldo " + saldo + " ja está disponível para saque!");
+		
+		
 	}
 
 }
